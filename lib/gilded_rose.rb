e816558f3addb662e4adca320ab8ec4a8b5f4a69
Jacob Cho <jacobcho@live.com>
2021-11-26 05:06:52 +0000

    all tests made.  conjured item added

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -18,6 +18,8 @@ class GildedRose
       BackstagePassesUpdater.new(item).update
     elsif item.name == "Aged Brie"
       AgedBrieUpdater.new(item).update
+    elsif item.name == "Conjured Item"
+      ConjuredItemUpdater.new(item).update
     else
       BasicItemUpdater.new(item).update
     end
@@ -100,6 +102,26 @@ class BasicItemUpdater
     if item.sell_in < 0 # Once the sell by date has passed, Quality degrades twice as fast
       if item.quality > 0
         item.quality = item.quality - 1
+      end
+    end
+  end
+end
+
+class ConjuredItemUpdater
+  attr_reader :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+    if item.quality > 0
+      item.quality = item.quality - 2
+    end
+    item.sell_in = item.sell_in - 1
+    if item.sell_in < 0 # Once the sell by date has passed, Quality degrades twice as fast
+      if item.quality > 0
+        item.quality = item.quality - 2
       end
     end
   end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -6,13 +6,85 @@ require_relative 'spec_helper'
 
 RSpec.describe GildedRose do
 
-  describe "#update_quality" do
-    it 'does not change the name' do
-      items = [Item.new("same-name", 0, 0)]
+  describe "#Sulfuras" do
+    it 'does not change at all' do
+      items = [Item.new("Sulfuras, Hand of Ragnaros", 100, 100)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "same-name"
+      expect(items[0].sell_in).to eq 100
+      expect(items[0].quality).to eq 100
     end
 
+  end
+
+  describe "#Backstage_Passes" do
+    it 'has -1 sell_in and +1 quality: 11 or more days until the concert.' do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 7)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 14
+      expect(items[0].quality).to eq 8
+    end
+
+    it 'has -1 sell_in and +2 quality: 10 Days left until the concert!' do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 44)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 9
+      expect(items[0].quality).to eq 46
+    end
+
+    it 'has -1 sell_in and +2 quality: 6 Days left until the concert!' do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 6, 44)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 5
+      expect(items[0].quality).to eq 46
+    end
+
+    it 'has -1 sell_in and +3 quality: 5 Days left until the concert!' do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 44)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 4
+      expect(items[0].quality).to eq 47
+    end
+
+
+    it 'has -1 sell_in and +3 quality: 1 Day left until the concert!' do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 1, 44)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 0
+      expect(items[0].quality).to eq 47
+    end
+
+    it 'has 0 quality value after the concert is over' do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 44)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq -1
+      expect(items[0].quality).to eq 0
+    end
+
+
+    it 'never has quality value above 50' do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 50)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 14
+      expect(items[0].quality).to eq 50
+    end
+  end
+
+
+  describe "#Aged_Brie" do
+    it 'has -1 sell_in and +1 quality' do
+      items = [Item.new("Aged Brie", 7, 7)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 6 
+      expect(items[0].quality).to eq 8
+    end
+
+
+    it 'never has quality value above 50' do
+      items = [Item.new("Aged Brie", 8, 50)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 7 
+      expect(items[0].quality).to eq 50
+    end
   end
 
 
@@ -40,24 +112,29 @@ RSpec.describe GildedRose do
 
   end
 
-  describe "#Aged_Brie" do
-    it 'has -1 sell_in and +1 quality' do
-      items = [Item.new("Aged Brie", 7, 7)]
+  describe "#conjured_item" do
+    it 'has -1 sell_in and -2 quality' do
+      items = [Item.new("Conjured Item", 7, 7)]
       GildedRose.new(items).update_quality()
       expect(items[0].sell_in).to eq 6 
-      expect(items[0].quality).to eq 8
+      expect(items[0].quality).to eq 5
     end
 
+    it 'has -1 sell_in and -4 quality: sell by date has passed, quality degrades double' do
+      items = [Item.new("Conjured Item", -1, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq -2 
+      expect(items[0].quality).to eq 6
+    end
 
-    it 'never has quality value above 50' do
-      items = [Item.new("Aged Brie", 8, 50)]
+    it 'never has negative quality value' do
+      items = [Item.new("Conjured Item", 8, 0)]
       GildedRose.new(items).update_quality()
       expect(items[0].sell_in).to eq 7 
-      expect(items[0].quality).to eq 50
+      expect(items[0].quality).to eq 0
     end
-  end
 
- 
+  end
 
   # Approvals ensure the test code behavior doesn't change, as we refactor GildedRose
   # https://github.com/kytrinyx/approvals
